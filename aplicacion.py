886d0ac8485ae6f1c831ac0bc276e6addc2bb3b9
gpanelli3 <gpanelli3@gmail.com>
2023-11-16 18:36:44 -0300

    'prueba'

--- aplicacion.py
+++ aplicacion.py
@@ -16,7 +16,7 @@ cursor.execute("select * from proveedores")
 proveedores=[]
 for i in cursor:
     proveedores.append(i)
-print(proveedores)
+#print(proveedores)
 conexion.close()
 #----------------------------------------------------------------------------
 conexion= mysql.connector.connect(host='localhost',
@@ -31,7 +31,7 @@ cursor2.execute("select * from cliente")
 
 for i in cursor2:
     clientes.append(i)
-print(clientes)
+#print(clientes)
 
 conexion.close()
 #----------------------------------------------------------------------------
@@ -52,14 +52,20 @@ def otra(request, response):
 
 
 @app.ruta("/clientes")
-def clientes(request,response):
+def cliente(request,response):
      response.text = app.template(
-    "clientes.html", context={"title": "Clientes", "user": "Lista de clientes","cliente":clientes})
+    "clientes.html", context={"title": "Clientes", "user": "Lista de clientes","cliente": clientes})
 
 
 
 @app.ruta("/altaclientes")
 def altaclientes(request, response):
+
+    conexion= mysql.connector.connect(host='localhost',
+                                  user='genaro',
+                                  passwd='password',
+                                  database='stock_control')
+    cursor3=conexion.cursor()
 
     nombre_cliente=request.POST.get('nombre')
     dni_cliente=request.POST.get('dni')
@@ -68,15 +74,28 @@ def altaclientes(request, response):
     sql="INSERT INTO cliente (nombre,dni,telefono) VALUES (%s,%s,%s)"
 
     datos_cliente=(nombre_cliente,dni_cliente,telefono_cliente)
-    cursor2.execute(sql,datos_cliente)
+    cursor3.execute(sql,datos_cliente)
     conexion.commit()
     conexion.close()
-
+    
     response.text=app.template(
         "altaclientes.html",context={"user": "Usuario Cargado"})
+    
+@app.ruta("/bajaclientes")
+def bajaclientes(request,response):
+    conexion= mysql.connector.connect(host='localhost',
+                                  user='genaro',
+                                  passwd='password',
+                                  database='stock_control')
+    cursor4=conexion.cursor()
 
+    #nombre=request.POST.get('nombre')
+    id=request.POST.get('id')
 
-
+    sql=("DELETE FROM cliente where cliente_id = %s",(id))
+    cursor4.execute(sql)
+    conexion.commit()
+    conexion.close()
 
 @app.ruta("/productos")
 def productos(request, response):
